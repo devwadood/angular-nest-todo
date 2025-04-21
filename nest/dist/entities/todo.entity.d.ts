9bcb55504bec9705e572ebd03a5b889d1e04de1b
ranaabdulwadoodrajpoot <devwadood@gmail.com>
2025-04-21 19:48:05 +0500

    Update gitignore and reset cache

--- nest/dist/entities/todo.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from './user.entity';
-export declare class Todo {
-    id: number;
-    title: string;
-    completed: boolean;
-    user: User;
-}
--- nest/dist/entities/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Todo } from './todo.entity';
-export declare class User {
-    id: number;
-    email: string;
-    password: string;
-    name: string;
-    todos: Todo[];
-}
